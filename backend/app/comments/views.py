1b6d47de192e28b11d2257e50e7a01c83b01bf91
mahmoud essam <esame4166@gmail.com>
2023-06-11 19:53:51 +0300

    Added comments to CRUD operations and implemented check for non-empty comment body

--- backend/app/comments/views.py
+++ backend/app/comments/views.py
@@ -5,7 +5,7 @@ from ..models import Comments, Users, Articles
 
 
 @comment.route('/comment/new', methods=['POST'])
-def new_comment():
+def new_comment() -> None:
     try:
 
         if request.method == 'POST':
@@ -15,6 +15,13 @@ def new_comment():
             body = data['body']
             commenter_id = data['commenter_id']
             article_id = data['article_id']
+
+            # check if the comment body is not empty.
+            if not body:
+                return jsonify({
+                    'success': False,
+                    'error': "Comment body can't be blank",
+                }), 400
 
             user = Users.query.get(int(commenter_id))
             article = Articles.query.get(int(article_id))
@@ -27,6 +34,71 @@ def new_comment():
             return jsonify({
                 'success': True,
                 'message': 'Comment created successfully',
+            }), 201
+
+    except KeyError:
+        # Invalid request
+        return jsonify({
+            'success': False,
+            'error': 'Invalid request',
+        }), 400
+    except Exception as e:
+        # Other errors handling
+        return jsonify({
+            'success': False,
+            'error': str(e)
+        }), 500
+
+
+@comment.route('/comment/edit/<int:comment_id>', methods=['PUT'])
+def edit_comment(comment_id) -> None:
+    try:
+
+        if request.method == 'PUT':
+
+            data = request.get_json()
+
+            comment_id = data['comment_id']
+            body = data['body']
+
+            comment = Comments.query.get(int(comment_id))
+
+            comment.body = body
+
+            comment.update()
+
+            return jsonify({
+                'success': True,
+                'message': 'Comment edited successfully',
+            }), 201
+
+    except KeyError:
+        # Invalid request
+        return jsonify({
+            'success': False,
+            'error': 'Invalid request',
+        }), 400
+    except Exception as e:
+        # Other errors handling
+        return jsonify({
+            'success': False,
+            'error': str(e)
+        }), 500
+
+
+@comment.route('/comment/delete/<int:comment_id>', methods=['DELETE'])
+def delete_comment(comment_id) -> None:
+    try:
+
+        if request.method == 'DELETE':
+
+            comment = Comments.query.get(int(comment_id))
+
+            comment.delete()
+
+            return jsonify({
+                'success': True,
+                'message': 'Comment deleted successfully',
             }), 201
 
     except KeyError:
